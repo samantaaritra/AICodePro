048e09abe9d1b33511f99b59983b229a1e7be13d
samantaaritra <iamsamantaaritra@gmail.com>
2025-08-18 22:04:43 +0530

    Add __init__.py to make src a package

---
+++ src/__init__.py
@@ -0,0 +1,16 @@
+"""
+AICodePro - AI Powered Code Optimization & Review Tool
+-------------------------------------------------------
+
+This package contains modules for:
+- Fetching PR data from GitHub
+- Cleaning and analyzing PRs
+- Labeling PRs for training
+- Training ML models
+- Predicting PR outcomes
+
+Author: Aritra Samanta
+"""
+
+__version__ = "0.1.0"
+
